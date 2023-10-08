7ab8187a11148fc87e578962f5a679be8b9f1e6d
Jason.Ou <jason.ou55@gmail.com>
2023-10-08 15:51:10 -0700

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <ChatGPT />
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import ChatGPT from './components/ChatGPT'
 
 export default {
